30bc9b6fa7d8871c082b2bce3f8c3a49ab44ddf8
919701 <fisapser@gmail.com>
2023-03-08 21:22:51 +0500

    feat: Adding validation to models

--- src/main/java/com/clevertec/CheckRunner/models/Product.java
+++ src/main/java/com/clevertec/CheckRunner/models/Product.java
@@ -1,6 +1,9 @@
 package com.clevertec.CheckRunner.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,9 +21,15 @@ public class Product{
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]*$",message = "Product name must contain letters, numbers")
     private String title;
+
+    @NotNull(message = "The price must be")
     @Positive(message = "Price product must be positive")
     private Double price;
-    private Boolean discount;
+    @Builder.Default
+    private Boolean discount = false;
 
 }
